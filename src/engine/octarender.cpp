d72c2231a76fff1aab29868ecf2b52681eff566c
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2013-06-10 13:32:45 +0000

    binormal -> bitangent
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@41 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/octarender.cpp
+++ src/engine/octarender.cpp
@@ -433,7 +433,7 @@ void reduceslope(ivec &n)
 }
 
 // [rotation][orient]
-extern const vec orientation_tangent [6][6] =
+extern const vec orientation_tangent[6][6] =
 {
     { vec( 0,  1,  0), vec( 0, -1,  0), vec(-1,  0,  0), vec( 1,  0,  0), vec( 1,  0,  0), vec( 1,  0,  0) },
     { vec( 0,  0, -1), vec( 0,  0, -1), vec( 0,  0, -1), vec( 0,  0, -1), vec( 0, -1,  0), vec( 0,  1,  0) },
@@ -442,7 +442,7 @@ extern const vec orientation_tangent [6][6] =
     { vec( 0, -1,  0), vec( 0,  1,  0), vec( 1,  0,  0), vec(-1,  0,  0), vec(-1,  0,  0), vec(-1,  0,  0) },
     { vec( 0,  1,  0), vec( 0, -1,  0), vec(-1,  0,  0), vec( 1,  0,  0), vec( 1,  0,  0), vec( 1,  0,  0) }
 };
-extern const vec orientation_binormal[6][6] =
+extern const vec orientation_bitangent[6][6] =
 {
     { vec( 0,  0, -1), vec( 0,  0, -1), vec( 0,  0, -1), vec( 0,  0, -1), vec( 0, -1,  0), vec( 0,  1,  0) },
     { vec( 0, -1,  0), vec( 0,  1,  0), vec( 1,  0,  0), vec(-1,  0,  0), vec(-1,  0,  0), vec(-1,  0,  0) },
@@ -665,7 +665,7 @@ void addcubeverts(VSlot &vslot, int orient, int size, vec *pos, int convex, usho
             t.sub(vec(n).mul(n.dot(t))).normalize();
             v.norm = bvec(n);
             v.tangent = bvec(t);
-            v.bitangent = vec().cross(n, t).dot(orientation_binormal[vslot.rotation][orient]) < 0 ? 0 : 255;
+            v.bitangent = vec().cross(n, t).dot(orientation_bitangent[vslot.rotation][orient]) < 0 ? 0 : 255;
         }
         else if(texture != DEFAULT_SKY)
         {
@@ -675,7 +675,7 @@ void addcubeverts(VSlot &vslot, int orient, int size, vec *pos, int convex, usho
             t.sub(vec(n).mul(n.dot(t))).normalize();
             v.norm = bvec(n);
             v.tangent = bvec(t);
-            v.bitangent = vec().cross(n, t).dot(orientation_binormal[vslot.rotation][orient]) < 0 ? 0 : 255;
+            v.bitangent = vec().cross(n, t).dot(orientation_bitangent[vslot.rotation][orient]) < 0 ? 0 : 255;
         }
         else
         {
