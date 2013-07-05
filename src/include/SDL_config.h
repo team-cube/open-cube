9785ef3c6a8ae1689db1a60480bd2160aedea850
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2013-07-05 07:42:41 +0000

    update to SDL2 RC1
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@137 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/include/SDL_config.h
+++ src/include/SDL_config.h
@@ -27,18 +27,18 @@
 /**
  *  \file SDL_config.h
  */
- 
+
 /* Add any platform that doesn't build using the configure system. */
 #if defined(__WIN32__)
 #include "SDL_config_windows.h"
 #elif defined(__MACOSX__)
 #include "SDL_config_macosx.h"
-#elif defined(__IPHONEOS__) 
+#elif defined(__IPHONEOS__)
 #include "SDL_config_iphoneos.h"
 #elif defined(__ANDROID__)
 #include "SDL_config_android.h"
-#elif defined(__NINTENDODS__)
-#include "SDL_config_nintendods.h"
+#elif defined(__PSP__)
+#include "SDL_config_psp.h"
 #else
 /* This is a minimal configuration just to get SDL running on new platforms */
 #include "SDL_config_minimal.h"
