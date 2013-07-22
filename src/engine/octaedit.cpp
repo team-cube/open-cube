acb3de12ed9dd5cbd91c2c48e5a8ef0c01ca96bc
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2013-07-22 10:41:13 +0000

    allow gettexname to work around of edit mode
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@222 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/octaedit.cpp
+++ src/engine/octaedit.cpp
@@ -1949,7 +1949,7 @@ void getseltex()
 
 void gettexname(int *tex, int *subslot)
 {
-    if(noedit(true) || *tex<0) return;
+    if(*tex<0) return;
     VSlot &vslot = lookupvslot(*tex, false);
     Slot &slot = *vslot.slot;
     if(!slot.sts.inrange(*subslot)) return;
