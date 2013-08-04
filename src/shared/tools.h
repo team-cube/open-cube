2596ad4c5023b68dc6ce1285eab9fad6c86457f2
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2013-08-04 12:47:40 +0000

    fix sortless strcmp template resolution
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@356 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/shared/tools.h
+++ src/shared/tools.h
@@ -332,6 +332,7 @@ static inline float heapscore(const T &n) { return n; }
 struct sortless
 {
     template<class T> bool operator()(const T &x, const T &y) const { return x < y; }
+    bool operator()(char *x, char *y) const { return strcmp(x, y) < 0; }
     bool operator()(const char *x, const char *y) const { return strcmp(x, y) < 0; }
 };
 
