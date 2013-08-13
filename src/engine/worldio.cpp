daa78e13553bdf18ae48e20563b613ddb3270e0c
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2013-08-13 21:21:18 +0000

    gametype fix
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@454 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/worldio.cpp
+++ src/engine/worldio.cpp
@@ -65,10 +65,10 @@ bool loadents(const char *fname, vector<entity> &ents, uint *crc)
     }
 
     string gametype;
-    gametype[0] = '\0';
     bool samegame = true;
     int len = f->getchar();
-    f->read(gametype, len+1);
+    if(len >= 0) f->read(gametype, len+1);
+    gametype[max(len, 0)] = '\0';
     if(strcmp(gametype, game::gameident()))
     {
         samegame = false;
@@ -759,10 +759,10 @@ bool load_world(const char *mname, const char *cname)        // still supports a
     if(dbgvars) conoutf(CON_DEBUG, "read %d vars", hdr.numvars);
 
     string gametype;
-    gametype[0] = '\0';
     bool samegame = true;
     int len = f->getchar();
-    f->read(gametype, len+1);
+    if(len >= 0) f->read(gametype, len+1);
+    gametype[max(len, 0)] = '\0';
     if(strcmp(gametype, game::gameident())!=0)
     {
         samegame = false;
