7b6314e210e8a6466399c3b989774b19abeff64d
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2013-08-18 13:18:01 +0000

    maxdupclients
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@545 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/server.cpp
+++ src/engine/server.cpp
@@ -103,6 +103,11 @@ void conoutf(int type, const char *fmt, ...)
 }
 #endif
 
+#define DEFAULTCLIENTS 8
+
+VARF(maxclients, 0, DEFAULTCLIENTS, MAXCLIENTS, { if(!maxclients) maxclients = DEFAULTCLIENTS; });
+VAR(maxdupclients, 0, 0, MAXCLIENTS);
+
 enum { ST_EMPTY, ST_LOCAL, ST_TCPIP };
 
 struct client                   // server side version of "dynent" type
@@ -165,6 +170,14 @@ void delclient(client *c)
         server::deleteclientinfo(c->info);
         c->info = NULL;
     }
+}
+
+bool checkdupclients(ENetPeer *peer)
+{
+    int dups = maxdupclients;
+    if(dups <= 0) return false;
+    loopv(clients) if(clients[i]->type == ST_TCPIP && clients[i]->peer->address.host == peer->address.host) dups--;
+    return dups <= 0;
 }
 
 void cleanupserver()
@@ -589,9 +602,6 @@ static int serverinfointercept(ENetHost *host, ENetEvent *event)
     return 1;
 }
 
-#define DEFAULTCLIENTS 8
-
-VARF(maxclients, 0, DEFAULTCLIENTS, MAXCLIENTS, { if(!maxclients) maxclients = DEFAULTCLIENTS; });
 VAR(serveruprate, 0, 0, INT_MAX);
 SVAR(serverip, "");
 VARF(serverport, 0, server::serverport(), 0xFFFF, { if(!serverport) serverport = server::serverport(); });
@@ -671,6 +681,11 @@ void serverslice(bool dedicated, uint timeout)   // main server update, called f
         {
             case ENET_EVENT_TYPE_CONNECT:
             {
+                if(checkdupclients(event.peer))
+                {
+                    enet_peer_disconnect_now(event.peer, DISC_MAXCLIENTS);
+                    break;
+                }
                 client &c = addclient(ST_TCPIP);
                 c.peer = event.peer;
                 c.peer->data = &c;
