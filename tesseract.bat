8e11a099b0f0eeb7f7c54d33e92242a8b36395b9
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2013-09-14 12:44:48 +0000

    check if 64 bit tesseract available before starting
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@607 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- tesseract.bat
+++ tesseract.bat
@@ -2,11 +2,13 @@
 
 set TESS_BIN=bin
 
-IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
-    set TESS_BIN=bin64
-)
-IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
-    set TESS_BIN=bin64
+IF EXIST bin64\tesseract.exe (
+    IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+        set TESS_BIN=bin64
+    )
+    IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+        set TESS_BIN=bin64
+    )
 )
 
 start %TESS_BIN%\tesseract.exe "-q$HOME\My Games\Tesseract" -glog.txt %*
