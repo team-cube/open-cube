c01c5fbf4f52c92b00a743c900940b25147853e6
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2014-01-03 15:38:40 +0000

    default modelpreviewpitch to -20
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@1172 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -2212,7 +2212,7 @@ void drawcubemap(int size, const vec &o, float yaw, float pitch, const cubemapsi
 }
 
 VAR(modelpreviewfov, 10, 40, 100);
-VAR(modelpreviewpitch, -90, -30, 90);
+VAR(modelpreviewpitch, -90, -20, 90);
 
 namespace modelpreview
 {
