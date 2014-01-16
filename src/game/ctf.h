68f1cfd3959abcfbcf345edd4dbf3fbc33784e2c
nieb <nieb@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2014-01-16 05:38:02 +0000

    src: flag icon wtf
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@1285 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/game/ctf.h
+++ src/game/ctf.h
@@ -376,8 +376,8 @@ struct ctfclientmode : clientmode
         {
             loopv(flags) if(flags[i].owner == d)
             {
-                int x = HICON_X + 3*HICON_STEP;
-                drawicon(flags[i].team==1 ? HICON_BLUE_FLAG : HICON_RED_FLAG, x, HICON_Y);
+                float x = 0.5f*(w); float y = 0.5f*(h); // Should this not be centered on the screen? (half screen-width, half screen-height)
+                drawicon(flags[i].team==1 ? HICON_BLUE_FLAG : HICON_RED_FLAG, x, y);
                 break;
             }
         }
@@ -393,7 +393,6 @@ struct ctfclientmode : clientmode
         float margin = 0.04f, roffset = s*margin, rsize = s + 2*roffset;
         setradartex();
         drawradar(x - roffset, y - roffset, rsize);
-        //#if 0
         settexture("media/interface/radar/compass.png", 3);
         pushhudmatrix();
         hudmatrix.translate(x - roffset + 0.5f*rsize, y - roffset + 0.5f*rsize, 0);
@@ -401,7 +400,6 @@ struct ctfclientmode : clientmode
         flushhudmatrix();
         drawradar(-0.5f*rsize, -0.5f*rsize, rsize);
         pophudmatrix();
-        //#endif
         drawplayerblip(d, x, y, s, 1.5f);
         loopv(flags)
         {
