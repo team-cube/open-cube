457d8182e334d975ffae8bfaacdb0dd3bd23852e
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2014-04-12 16:10:10 +0000

    amd_eal_bug typo fix
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@1572 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/rendergl.cpp
+++ src/engine/rendergl.cpp
@@ -1009,7 +1009,7 @@ void gl_checkextensions()
         msaalineardepth = glineardepth = 1; // reading back from depth-stencil still buggy on newer cards, and requires stencil for MSAA
         msaadepthstencil = gdepthstencil = 1; // some older AMD GPUs do not support reading from depth-stencil textures, so only use depth-stencil renderbuffer for now
         if(checkseries(renderer, "Radeon HD", 4000, 5199)) amd_pf_bug = 1;
-        if(glversion <= 33) amd_eal_bug = 1;
+        if(glversion <= 330) amd_eal_bug = 1;
         rhrect = 1; // bad cpu stalls on Catalyst 13.x when trying to use 3D textures previously bound to FBOs
     }
     else if(nvidia)
