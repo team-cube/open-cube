05983ff8b7851e0368cf2b13c351cafa5ff0c17a
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2014-05-16 08:11:17 +0000

    tryspawn fix
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@1684 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/game/server.cpp
+++ src/game/server.cpp
@@ -2966,6 +2966,7 @@ namespace server
                 {
                     ci->mapcrc = -1;
                     checkmaps();
+                    if(ci == cq ? ci->state.state!=CS_DEAD : cq->ownernum != ci->clientnum) break;
                 }
                 if(cq->state.deadflush)
                 {
