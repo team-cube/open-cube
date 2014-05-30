5476fb83d10e009b242ad986ac5cf4beaa0b474a
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2014-05-30 19:30:26 +0000

    maxdemosize 31MB
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@1702 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/game/server.cpp
+++ src/game/server.cpp
@@ -602,7 +602,7 @@ namespace server
     int nextplayback = 0, demomillis = 0;
 
     VAR(maxdemos, 0, 5, 25);
-    VAR(maxdemosize, 0, 16, 32);
+    VAR(maxdemosize, 0, 16, 31);
     VAR(restrictdemos, 0, 1, 1);
 
     VAR(restrictpausegame, 0, 1, 1);
