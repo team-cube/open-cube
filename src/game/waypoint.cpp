2c97cde1da58ba13f2f3bbf036549779c724d9bb
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2014-07-31 21:39:30 +0000

    skip empty wpcaches
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@1799 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/game/waypoint.cpp
+++ src/game/waypoint.cpp
@@ -223,7 +223,7 @@ namespace ai
         } while(0)
         int closest = -1;
         wpcache::node *curnode;
-        loop(which, NUMWPCACHES) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
+        loop(which, NUMWPCACHES) if(wpcaches[which].nodes.length()) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
         {
             int axis = curnode->axis();
             float dist1 = pos[axis] - curnode->split[0], dist2 = curnode->split[1] - pos[axis];
@@ -277,7 +277,7 @@ namespace ai
             } \
         } while(0)
         wpcache::node *curnode;
-        loop(which, NUMWPCACHES) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
+        loop(which, NUMWPCACHES) if(wpcaches[which].nodes.length()) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
         {
             int axis = curnode->axis();
             float dist1 = pos[axis] - curnode->split[0], dist2 = curnode->split[1] - pos[axis];
@@ -329,7 +329,7 @@ namespace ai
             } \
         } while(0)
         wpcache::node *curnode;
-        loop(which, NUMWPCACHES) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
+        loop(which, NUMWPCACHES) if(wpcaches[which].nodes.length()) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
         {
             int axis = curnode->axis();
             float dist1 = pos[axis] - curnode->split[0], dist2 = curnode->split[1] - pos[axis];
