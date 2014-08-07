839f470959baf26929365e503012683750e06e67
eihrul <eihrul@b412e182-2ed4-4078-95c4-27dc59f57fa9>
2014-08-07 21:23:44 +0000

    remove more persistent vars
    git-svn-id: svn://svn.tuxfamily.org/svnroot/tesseract/main@1807 b412e182-2ed4-4078-95c4-27dc59f57fa9

--- src/engine/rendermodel.cpp
+++ src/engine/rendermodel.cpp
@@ -538,7 +538,7 @@ static inline void renderbatchedmodel(model *m, const batchedmodel &b)
     m->render(anim, b.basetime, b.basetime2, b.pos, b.yaw, b.pitch, b.roll, b.d, a, b.sizescale, b.colorscale);
 }
 
-VARP(maxmodelradiusdistance, 10, 200, 1000);
+VAR(maxmodelradiusdistance, 10, 200, 1000);
 
 static inline void enablecullmodelquery()
 {
